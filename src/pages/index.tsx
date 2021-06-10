39e30f9be611f6785b8860f02efd9e19cabeee22
callseven <callseven@me.com>
2021-06-09 23:33:02 -0300

    first commit React

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+
+export default function Home() {
+  return (
+    <div></div>
+  )
+}
---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const SafeEnvironment = () => {
+    return <div>Ambiente Seguro</div>
+}
+
+export default SafeEnvironment;
